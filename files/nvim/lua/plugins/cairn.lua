b84f06c82acf0dde667858610cee877e22cbe8b3
Cameron Mostert <cameron.mostert@flash.co.za>
2026-09-09 17:07:19 +0200

    cairn config updates

--- files/nvim/lua/plugins/cairn.lua
+++ files/nvim/lua/plugins/cairn.lua
@@ -1,16 +1,32 @@
 return {
-  "GooseRooster/cairn.nvim",
-  dependencies = {
-    "folke/which-key.nvim",
-  },
-  config = function()
-    require("cairn").setup({
+	{
+		"GooseRooster/cairn.nvim",
+		dependencies = {
+			"folke/which-key.nvim",
+		},
+		config = function()
+			require("cairn").setup({
 
-      track_cursor = true,
-    })
+				track_cursor = true,
+				arena = {
+					enabled = true,
+					max_items = 10,
+					persist = true, -- persist per-workspace to <key>_arena.json
+					scope_to_workspace = true, -- hide entries tracked in other workspaces
+					algorithm = {
+						recency_factor = 0.5, -- weight given to how recently a file was visited
+						frequency_factor = 1, -- weight given to how often a file was visited
+					},
+				},
+			})
 
-    require("which-key").add({
-      { "<leader>m", group = "cairn", icon = "󰔷" },
-    })
-  end,
+			require("which-key").add({
+				{ "<leader>m", group = "cairn", icon = "󰔷" },
+			})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		enabled = false,
+	},
 }
